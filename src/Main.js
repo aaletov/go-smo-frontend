4f8ffa4464e16b22378b9cf83f9ec9963fe49b0a
aaletov <aapozdnyakove@gmail.com>
2022-11-17 15:45:44 +0300

    MUI and routing
    Signed-off-by: aaletov <aapozdnyakove@gmail.com>

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,21 @@
-import React, { Component } from "react";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { Component } from 'react';
 import {
   Route,
   NavLink,
@@ -9,28 +26,82 @@ import Home from "./Home";
 import Stuff from "./Stuff";
 import Contacts from "./Contacts";
 
+const drawerWidth = 240;
+
 class Main extends Component {
-    render() {
-      return (
-        <HashRouter>
-          <div>
-            <h1>Simple SPA</h1>
-            <ul className="header">
-              <li><NavLink to="/">Home</NavLink></li>
-              <li><NavLink to="/stuff">Stuff</NavLink></li>
-              <li><NavLink to="/contacts">Contact</NavLink></li>
-            </ul>
-            <div className="content">
-              <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/stuff" element={<Stuff/>}/>
-                <Route path="/contacts" element={<Contacts/>}/>
-              </Routes>
-            </div>
-          </div>
-        </HashRouter>
-      );
-    }
+  render() {
+    //const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    // const handleDrawerToggle = () => {
+    //   setMobileOpen(!mobileOpen);
+    // };
+    let links = ["/", "/stuff", "/contacts"]
+    const drawer = (
+      <div>
+        <Toolbar />
+        <Divider />
+        <List>
+          {['Home', 'Waveform', 'Svod'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <NavLink to={links[index]}>
+                  <ListItemText primary={text} />
+                </NavLink>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    );
+
+    const container = document.getElementById('root')
+
+    return (
+      <HashRouter>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          sx={{
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            ml: { sm: `${drawerWidth}px` },
+          }}
+        >
+        </AppBar>
+        <Box
+          component="nav"
+          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+          aria-label="mailbox folders"
+        >
+          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+          <Drawer
+            variant="permanent"
+            sx={{
+              display: { xs: 'none', sm: 'block' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        >
+          <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route path="/stuff" element={<Stuff/>}/>
+            <Route path="/contacts" element={<Contacts/>}/>
+          </Routes> 
+        </Box>
+      </Box>
+      </HashRouter>
+    );
+  }
 }
 
 export default Main;
